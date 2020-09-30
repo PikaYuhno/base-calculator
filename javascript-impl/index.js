db9a9141981ce28bce2fc1ad2ebed1c3bc50e062
PikaYuhno <muazahmed019@gmail.com>
2020-09-30 18:51:28 +0200

    finished baseToNumber;

--- javascript-impl/index.js
+++ javascript-impl/index.js
@@ -1,3 +1,12 @@
+//TODO: Change the name to something more appropriate.
+let map = {
+    A: 10,
+    B: 11,
+    C: 12,
+    D: 13,
+    E: 14,
+    F: 15,
+};
 /**
  * This function converts any base that is smaller than 10 to a number.
  * @param {number} input - The input number.
@@ -11,6 +20,11 @@ const baseToNumber = (input, base) => {
     const shiftedInput = characteristicPart + mantissaPart;
     let number = 0;
     for (let i = 0; i < shiftedInput.length; i++) {
+        if (isNaN(shiftedInput[i])) {
+            number +=
+                map[shiftedInput[i]] * base ** (shiftedInput.length - i - 1);
+            continue;
+        }
         number += shiftedInput[i] * base ** (shiftedInput.length - i - 1);
     }
 
@@ -37,12 +51,12 @@ const numberToBase = (input, base) => {
 
     let secnumber = "";
     while (mantissaPart % 1 != 0) {
-        mantissaPart*=base;
+        mantissaPart *= base;
         secnumber += Math.floor(mantissaPart);
         mantissaPart = mantissaPart - Math.floor(mantissaPart);
     }
     secnumber += Math.floor(mantissaPart);
 
     number = number.split("").reverse().join("");
-    return number+"."+secnumber;
+    return number + "." + secnumber;
 };
