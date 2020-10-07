d3ca2694e1bcf77e4a5a886f70bbc1769a5dc81d
PikaYuhno <muazahmed019@gmail.com>
2020-10-07 15:43:16 +0200

    Removed unnecessary

--- javascript-impl/index.js
+++ javascript-impl/index.js
@@ -36,8 +36,6 @@ const baseToNumber = (input, base) => {
  * @param {number} input - The input number.
  * @param {number} base - The base.
  */
-//TODO
-// + Convert number with decimal part.
 const numberToBase = (input, base) => {
     let characteristicPart = Math.floor(input);
     let mantissaPart = input - Math.floor(input);
@@ -61,144 +59,141 @@ const numberToBase = (input, base) => {
     return number + "." + secnumber;
 };
 
-const numberAddition = (input1,input2,base) => {
+const numberAddition = (input1, input2, base) => {
     let splitted1 = input1.split(".");
     let splitted2 = input2.split(".");
 
-    if(splitted1[1].length<splitted2[1].length){
-        [input1,input2] = [input2,input1];
-        [splitted1,splitted2] = [splitted2,splitted1];
+    if (splitted1[1].length < splitted2[1].length) {
+        [input1, input2] = [input2, input1];
+        [splitted1, splitted2] = [splitted2, splitted1];
     }
 
-    for(var i=0;i<splitted1[1].length-splitted2[1].length;i++){
-        input2+="0";
+    for (var i = 0; i < splitted1[1].length - splitted2[1].length; i++) {
+        input2 += "0";
     }
 
-    if(splitted1[0].length<splitted2[0].length){
-        [input1,input2] = [input2,input1];
-        [splitted1,splitted2] = [splitted2,splitted1];
+    if (splitted1[0].length < splitted2[0].length) {
+        [input1, input2] = [input2, input1];
+        [splitted1, splitted2] = [splitted2, splitted1];
     }
 
-    for(var i=0;i<splitted1[0].length-splitted2[0].length;i++){
+    for (var i = 0; i < splitted1[0].length - splitted2[0].length; i++) {
         input2 = "0" + input2;
     }
 
-    let result="";
-    let transfer=0;
-    for(var i=input1.length-1;i>=0;i--){
-        if(input1[i]=="."){
-            result+=".";
+    let result = "";
+    let transfer = 0;
+    for (var i = input1.length - 1; i >= 0; i--) {
+        if (input1[i] == ".") {
+            result += ".";
             continue;
         }
 
-        var num1=parseInt(input1[i]);
-        var num2=parseInt(input2[i]);
+        var num1 = parseInt(input1[i]);
+        var num2 = parseInt(input2[i]);
 
-        result+=""+((num1+num2+transfer)%base);
-        transfer=Math.floor((num1+num2+transfer)/base);
+        result += "" + ((num1 + num2 + transfer) % base);
+        transfer = Math.floor((num1 + num2 + transfer) / base);
     }
 
-    if(transfer!=0){
-        result+=""+transfer;
+    if (transfer != 0) {
+        result += "" + transfer;
     }
 
     return result.split("").reverse().join("");
 };
 
-const NumberSubtraction = (input1,input2,base) => {
-    let copy1=input1;
+const NumberSubtraction = (input1, input2, base) => {
+    let copy1 = input1;
 
     let splitted1 = input1.split(".");
     let splitted2 = input2.split(".");
 
-    if(splitted1[1].length<splitted2[1].length){
-        [input1,input2] = [input2,input1];
-        [splitted1,splitted2] = [splitted2,splitted1];
+    if (splitted1[1].length < splitted2[1].length) {
+        [input1, input2] = [input2, input1];
+        [splitted1, splitted2] = [splitted2, splitted1];
     }
 
-    for(var i=0;i<splitted1[1].length-splitted2[1].length;i++){
-        input2+="0";
+    for (var i = 0; i < splitted1[1].length - splitted2[1].length; i++) {
+        input2 += "0";
     }
 
-    if(splitted1[0].length<splitted2[0].length){
-        [input1,input2] = [input2,input1];
-        [splitted1,splitted2] = [splitted2,splitted1];
+    if (splitted1[0].length < splitted2[0].length) {
+        [input1, input2] = [input2, input1];
+        [splitted1, splitted2] = [splitted2, splitted1];
     }
 
-    for(var i=0;i<splitted1[0].length-splitted2[0].length;i++){
+    for (var i = 0; i < splitted1[0].length - splitted2[0].length; i++) {
         input2 = "0" + input2;
     }
 
-    if(!input1.includes(copy1)){
-        [input1,input2] = [input2,input1];
+    if (!input1.includes(copy1)) {
+        [input1, input2] = [input2, input1];
     }
 
-    let result="";
-    let transfer=0;
-    let temp=0;
-    let minus="";
+    let result = "";
+    let transfer = 0;
+    let temp = 0;
+    let minus = "";
 
-    if(parseFloat(input1)<parseFloat(input2)){
-        [input1,input2] = [input2,input1];
-        minus="-";
+    if (parseFloat(input1) < parseFloat(input2)) {
+        [input1, input2] = [input2, input1];
+        minus = "-";
     }
 
-    for(var i=input1.length-1;i>=0;i--){
-        if(input1[i]=="."){
-            result+=".";
+    for (var i = input1.length - 1; i >= 0; i--) {
+        if (input1[i] == ".") {
+            result += ".";
             continue;
         }
 
-        var num1=parseInt(input1[i]);
-        var num2=parseInt(input2[i]);
+        var num1 = parseInt(input1[i]);
+        var num2 = parseInt(input2[i]);
 
-        if(num1<num2+transfer){
-            num1+=base;
-            temp=1;
+        if (num1 < num2 + transfer) {
+            num1 += base;
+            temp = 1;
         }
 
-        result+=""+num1-(num2+transfer);
-        transfer=temp;
-        temp=0;
+        result += "" + num1 - (num2 + transfer);
+        transfer = temp;
+        temp = 0;
     }
 
-    result=result.split("").reverse().join("");
+    result = result.split("").reverse().join("");
 
-    if(minus=="-"){
-        return minus+result.substring(1);
+    if (minus == "-") {
+        return minus + result.substring(1);
     }
     return result;
 };
 
-const numberDivision = (input1,input2,base) => {
+const numberDivision = (input1, input2, base) => {
+    let number = "";
+    let result = "";
 
-    let number="";
-    let result="";
+    for (var i = 0; i < input1.length; i++) {
+        number += "" + input1[i];
 
-    for(var i=0;i<input1.length;i++){
-        number+=""+input1[i];
-
-        if(input1[i]=="."){
-            result+=".";
+        if (input1[i] == ".") {
+            result += ".";
             continue;
         }
 
-
-        if(parseFloat(number)>=input2){
-            let divisor=input2;
-            let a=1;
-            let previous=input2;
-            while(parseFloat(number)>divisor){
-                previous=divisor;
-                divisor=numberAddition(divisor+".",input2+".",base);
+        if (parseFloat(number) >= input2) {
+            let divisor = input2;
+            let a = 1;
+            let previous = input2;
+            while (parseFloat(number) > divisor) {
+                previous = divisor;
+                divisor = numberAddition(divisor + ".", input2 + ".", base);
                 a++;
             }
-            number=numberSubtraction(number+".",previous+".",base);
-    
-            result+=""+a;
+            number = numberSubtraction(number + ".", previous + ".", base);
+
+            result += "" + a;
         }
     }
-
 
     return result;
 };
